46eb6f6d2ec0c678b930806036c3c3e5066c10cf
NaufalFerdiansyah <112892244+NaufalFerdiansyah@users.noreply.github.com>
2022-09-09 21:29:57 +0700

    Add files via upload

---
+++ Tugas1/tugas1/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Naufal Ferdiansyah WIrahutama \nA11.2022.14652.14652 \nLaki-Laki \nSMAN 12 Semarang" << endl;
+    return 0;
+}
