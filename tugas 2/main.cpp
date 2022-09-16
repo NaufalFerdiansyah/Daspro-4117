34bdce90336dd05851af56bf5d2dcb5154358bb7
NaufalFerdiansyah <112892244+NaufalFerdiansyah@users.noreply.github.com>
2022-09-16 22:32:03 +0700

    Add files via upload

---
+++ tugas 2/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    //instalasi atau deklarasi
+    int a= 4117;
+    int b= 18;
+    float c= 166.6;
+    string d= "XL";
+    string e= "Naufal Ferdiansyah Wirahutama";
+    string f= "A11.2022.14652";
+    string g= "Semarang, 8 April 2004";
+    string h= "Jl.Seudati VII Blok C35 Perum P4A, Pudak Payung, Banyumanik, Semarang";
+    string i= "Otomotif, Music, sejarah, arsitektur";
+
+    //output identitas
+    cout << "HALO KAWAN KAWAN SEJAWAT SETANAH AIR" << endl;
+
+    printf("\n\nIdentitas");
+    cout << "\nNama     :   " << e;
+    cout << "\nNim      :   " << f;
+    cout << "\nKelompok :   " << a;
+
+    //data pribadi
+    printf("\n\nData Pribadi");
+    cout << "\nTinggi                :   " << c;
+    cout << "\nUmur                  :   " << b;
+    cout << "\nUkuran Baju           :   " << d;
+    cout << "\nTempat Tanggal Lahir  :   " << g;
+    cout << "\nAlamat                :   " << h;
+    cout << "\nHobi                  :   " << i;
+
+    printf("\n\nDaspro Pertemuan 3");
+    return 0;
+}
