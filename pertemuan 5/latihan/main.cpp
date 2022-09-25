39dfa8de35364a992822fce7f09c13d622194846
NaufalFerdiansyah <112892244+NaufalFerdiansyah@users.noreply.github.com>
2022-09-25 21:50:07 +0700

    Add files via upload

---
+++ pertemuan 5/latihan/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int hasil;
+    cout << "Pemrograman luas lingkaran" << endl;
+    cout << "Luas lingkaran diameter 20" << endl;
+    cout << "jari jari= ";
+    cin >> x;
+    float phi = 3.14;
+    hasil = phi * x * x;
+    cout << "hasil: " << hasil;
+
+    return 0;
+}
