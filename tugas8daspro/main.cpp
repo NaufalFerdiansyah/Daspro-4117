613f1fed7ca121752ac890865f28efa7d75cb22d
NaufalFerdiansyah <112892244+NaufalFerdiansyah@users.noreply.github.com>
2022-10-08 22:28:00 +0700

    Add files via upload

---
+++ tugas8daspro/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "Program Mencari angka Kelipatan 5" << endl;
+
+    int a;
+
+    a=5;
+    while(a<=10000){
+        cout<<a<<endl;
+        a+=5;
+    }
+    return 0;
+}
